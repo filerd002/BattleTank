21c8d70efe94f2f344b699e315d0a6d3645da2d3
Mikołaj Dalecki <dalecki.mikolaj@gmail.com>
2018-04-26 11:54:09 +0200

    Dodano wstępną klasę kamery 2D.

---
+++ BattleTank.Core/Camera2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace BattleTank.Core
+{
+    class Camera2D
+    {
+        public double Scale { get; set; }
+        public double Position { get; set; }
+
+        public Point? MaxLeftTopCorner { get; set; } = null;
+
+        public Point? MaxRightBottomCorner { get; set; } = null;
+
+        public Matrix GetViewMatrix()
+            => Camera2D.GetViewMatrix(Scale, Position, MaxLeftTopCorner, MaxRightBottomCorner);
+
+        public static Matrix GetViewMatrix(double scale, double position, Point? maxLeftTopCorner = null,
+            Point? maxRightBottomCorner = null)
+        {
+            return new Matrix();
+        }
+    }
+}

214c6ec6be2cacbe4127cbdcac420872a81574f0
Mikołaj Dalecki <dalecki.mikolaj@gmail.com>
2018-04-26 16:16:11 +0200

    Przeniesiono funkcje rysujace tekst po Texture2D do osobnej funkcji.

--- BattleTank.Core/GUI/Button.cs
+++ BattleTank.Core/GUI/Button.cs
@@ -14,7 +14,7 @@ namespace BattleTank.Core.GUI
         Texture2D NonActiveTexture { get; set; }
         Texture2D ActiveTexture { get; set; }
         
-
+        [Obsolete]
         public Button(Texture2D nonActiveTexture, Texture2D activeTexture, Rectangle elementRectangle) :base(nonActiveTexture)
         {
             NonActiveTexture = nonActiveTexture;
@@ -26,38 +26,12 @@ namespace BattleTank.Core.GUI
 
         public Button(string text, Vector2 position, int? width, int? height) : base(null)
         {
-            var stringDiamensions = UIElement.InActiveFont.MeasureString(text);
-                RenderTarget2D renderTargetForInActive = new RenderTarget2D(
-                GraphicsDevice,
-                (int)stringDiamensions.X,
-                (int)stringDiamensions.Y);
-            GraphicsDevice.Clear(Color.White);
-
-            var spriteBatch = new SpriteBatch(GraphicsDevice);
-            spriteBatch.GraphicsDevice.SetRenderTarget(renderTargetForInActive);
-            GraphicsDevice.Clear(Color.Transparent);
-            spriteBatch.Begin();
-            spriteBatch.DrawString(InActiveFont, text, Vector2.Zero, Color.White);
-            spriteBatch.End();
-
-            NonActiveTexture = renderTargetForInActive;
-
-            RenderTarget2D renderTargetForActive = new RenderTarget2D(
-                GraphicsDevice,
-                (int)stringDiamensions.X,
-                (int)stringDiamensions.Y);
-            spriteBatch.GraphicsDevice.SetRenderTarget(renderTargetForActive);
-            GraphicsDevice.Clear(Color.Transparent);
-            spriteBatch.Begin();
-            spriteBatch.DrawString(ActiveFont, text, Vector2.Zero, Color.White);
-            spriteBatch.DrawString(InActiveFont, text, Vector2.Zero, Color.White);
-            spriteBatch.End();
-            ActiveTexture = renderTargetForActive;
-
-
-            spriteBatch.GraphicsDevice.SetRenderTarget(null);
+            NonActiveTexture = GUIHelper.DrawStringOnTexture2D(text, UIElement.InActiveFont, null, GraphicsDevice);
+            
+            ActiveTexture = GUIHelper.DrawStringOnTexture2D(text, UIElement.InActiveFont, UIElement.ActiveFont, GraphicsDevice);
 
             UIElementRectangle.Location = position.ToPoint();
+
             base.Width = width ?? NonActiveTexture.Width;
             base.Height = height ?? NonActiveTexture.Height;
         }
---
+++ BattleTank.Core/GUI/GUIHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BattleTank.Core.GUI
+{
+    class GUIHelper
+    {
+        public static Texture2D DrawStringOnTexture2D(string text, SpriteFont foreGroundFont, SpriteFont backgroundFont, GraphicsDevice graphicsDevice)
+        {
+            var stringDiamensions = UIElement.InActiveFont.MeasureString(text);
+            RenderTarget2D renderTarget2D = new RenderTarget2D(graphicsDevice, (int)stringDiamensions.X, (int)stringDiamensions.Y);
+
+            var spriteBatch = new SpriteBatch(graphicsDevice);
+            spriteBatch.GraphicsDevice.SetRenderTarget(renderTarget2D);
+
+            graphicsDevice.Clear(Color.Transparent);
+
+            spriteBatch.Begin();
+            if (backgroundFont != null)
+                spriteBatch.DrawString(backgroundFont, text, Vector2.Zero, Color.White);
+            spriteBatch.DrawString(foreGroundFont, text, Vector2.Zero, Color.White);
+            spriteBatch.End();
+
+            spriteBatch.GraphicsDevice.SetRenderTarget(null);
+
+            return renderTarget2D;
+        }
+    }
+}
